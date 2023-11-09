8a95befdc16224343c1bd5d09fa1daee4a81cd86
Ankit Sharma <sharmaa7861@gmail.com>
2023-11-10 02:17:57 +0530

    started: TLE DP

---
+++ DP/Dice_Combinations.cpp
@@ -0,0 +1,15 @@
+/*
+Your task is to count the number of ways to construct sum n by throwing a dice one or more times. Each throw produces an outcome between 1 and 6.
+for example, if n=0, there are 4 ways:
+- 1+1+1
+- 1+2
+- 2+1
+- 3
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
