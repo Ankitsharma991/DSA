2eb0bda59346515edb4bfb575e3ba8aefbb830ce
Ankit Sharma <sharmaa7861@gmail.com>
2023-11-28 03:00:28 +0530

    started stack cpp

--- stackC++/ReverseSentence.cpp
+++ stackC++/ReverseSentence.cpp
@@ -1,6 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+
+void reverseSentence(string s)
+{
+    stack<string> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        string word = "";
+        while (s[i] != ' ' && i < s.length())
+        {
+            word += s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+    string s = "Hey, how are you doing ?";
+    cout << s << endl;
+    reverseSentence(s);
     return 0;
 }
